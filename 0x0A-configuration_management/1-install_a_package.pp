8c64f8bfc2a38a0c8ac2a085b6acf2db5b3535b8
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-03-25 19:12:17 +0200

    lastupdated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,6 @@
-# Define the package for Python 3 pip
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask version 2.1.0 using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0$"',
-}
-
-# Notify user that Flask installation is complete
-notify { 'Flask installed successfully':
-  require => Exec['install_flask'],
+#!/usr/bin/pup
+#install a specific version of flask (2.1.0)
+package {'flask':
+	ensure  => '2.1.0',
+	provider  => 'pip'
 }
