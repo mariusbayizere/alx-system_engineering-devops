02d7b57e881d2d2c9a561646509482cfaf2fae52
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-03-27 02:02:41 +0200

    web-server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# install and configure an Nginx server using Puppet instead of Bash.
+
+exec { 'update system':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update system'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+exec { 'redirect_me':
+  command => 'sed -i "24i\\ rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
---
+++ 0x0C-web_server/README.md
@@ -0,0 +1,1 @@
+This the readme file of the webserver project 
