8ba5d11729e917b9cdc149ea8b41e1d56d2ea24d
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-04-19 22:09:19 +0200

    adding

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,16 @@
-# Puppet manifest to install Flask version 2.1.0 using pip3
-
+# Define the package for Python 3 pip
 package { 'python3-pip':
   ensure => installed,
 }
-
+# Install Flask version 2.1.0 using pip3
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/usr/bin'],
-  require => Package['python3-pip'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0$"',
 }
+# Notify user that Flask installation is complete
+notify { 'Flask installed successfully':
+  require => Exec['install_flask'],
+}
+
 
