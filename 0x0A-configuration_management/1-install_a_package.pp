ca929ef118de69bbfa765598ee47505934f0bd70
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-04-19 18:55:52 +0200

    puppet-limit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,14 +2,9 @@
 package { 'python3-pip':
   ensure => installed,
 }
-# Install Flask version 2.1.0 using pip3
+
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/usr/bin'],
   unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0$"',
 }
-# Notify user that Flask installation is complete
-notify { 'Flask installed successfully':
-  require => Exec['install_flask'],
-}
-
---
+++ 0x0F-load_balancer/README.md
@@ -0,0 +1,1 @@
+ Readme file for this webserver loading balancer 
