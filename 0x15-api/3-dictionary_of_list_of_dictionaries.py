37bb090e220b02162c4ae29606939de87e5f5a97
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-05-21 09:00:38 +0200

    Task3-push

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""
-Using what you did in the task #0, extend your Python script to export
-data in the JSON format.
-"""
-
+"""Export to JSON"""
 import json
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
-    user_id = argv[1]
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    todos_url = ('https://jsonplaceholder.typicode.com/users/{}/todos'
-                 ''.format(user_id))
+    users = requests.get(
+        "https://jsonplaceholder.typicode.com/users").json()
+    all_todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos").json()
 
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
+    tasks_dict = {}
+    for user in users:
+        user_id = str(user.get("id"))
+        username = user.get("username")
+        tasks = [
+            {
+                "username": username,
+                "task": task.get("title"),
+                "completed": task.get("completed")
+            }
+            for task in all_todo if task.get("userId") == user.get("id")
+        ]
+        tasks_dict[user_id] = tasks
 
-    username = user_response.json().get('username')
-    todos = todos_response.json()
-
-    user_tasks = {user_id: []}
-
-    for task in todos:
-        user_tasks[user_id].append({
-            "username": username,
-            "task": task.get("title"),
-            "completed": task.get("completed"),
-        })
-
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump(user_tasks, jsonfile)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(tasks_dict, jsonfile)
