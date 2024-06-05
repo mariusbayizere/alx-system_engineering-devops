8668a17594056869cd3ef2b20f469c961e0bda02
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-06-05 16:22:22 +0200

    webdebugg

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,41 +1,6 @@
-class apache_fix {
-  # Ensure apache2 package is installed
-  package { 'apache2':
-    ensure => installed,
-  }
-
-  # Ensure apache2 service is running and enabled at boot
-  service { 'apache2':
-    ensure     => running,
-    enable     => true,
-    subscribe  => Exec['apache-configtest'],
-  }
-
-  # Ensure proper permissions for web content directory
-  file { '/var/www/html':
-    ensure  => directory,
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0755',
-    recurse => true,
-  }
-
-  # Ensure apache configuration syntax is correct
-  exec { 'apache-configtest':
-    command     => '/usr/sbin/apachectl configtest',
-    refreshonly => true,
-    notify      => Service['apache2'],
-  }
-
-  # Restart Apache if any of the files change
-  exec { 'restart-apache':
-    command     => '/usr/sbin/systemctl restart apache2',
-    refreshonly => true,
-    subscribe   => [
-      File['/var/www/html'],
-    ],
-  }
+# Restart Apache if any of the files change
+{ 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
-
-# Include the class
-include apache_fix
