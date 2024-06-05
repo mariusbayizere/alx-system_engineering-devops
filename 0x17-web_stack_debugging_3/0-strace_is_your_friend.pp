c54eddfca59039949fee5016f3a6a7fd86b02930
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-06-05 21:41:16 +0200

    last-one

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,21 @@
-# Restart Apache if any of the files change
-{ 'fix-wordpress':
-  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
-/var/www/html/wp-settings.php; service apache2 restart"',
-  path    => '/usr/bin:/usr/sbin:/bin'
+# 0-strace_is_your_friend.pp
+# This Puppet manifest installs PHP and the missing php-mysql module to fix the 500 Internal Server Error in Apache
+
+exec { 'install_php':
+  command => '/usr/bin/apt-get update && /usr/bin/apt-get install -y php',
+  path    => ['/usr/bin', '/usr/sbin'],
+  unless  => 'which php',
+}
+
+exec { 'install_php_mysql':
+  command => '/usr/bin/apt-get install -y php-mysql',
+  path    => ['/usr/bin', '/usr/sbin'],
+  unless  => '/usr/bin/php -m | grep mysql',
+  require => Exec['install_php'],
+}
+
+service { 'apache2':
+  ensure  => 'running',
+  enable  => true,
+  require => Exec['install_php_mysql'],
 }
