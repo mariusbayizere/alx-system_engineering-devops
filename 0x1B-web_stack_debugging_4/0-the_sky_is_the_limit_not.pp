edabe2aa8dade34c6471478fde7c7a940e3499a9
bayizeremarius119@gmail.com <bayizeremarius119@gmail.com>
2024-06-13 12:55:38 +0200

    first-modification

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,16 @@
-# Fixes nginx configuration to handle higher load by increasing worker connections and restarting the service
-exec { 'fix--for-nginx':
-  command => "bash -c \"sed -iE 's/worker_connections [0-9]\\+/worker_connections 1024/' /etc/nginx/nginx.conf && \
-sed -iE 's/worker_processes [0-9]\\+/worker_processes auto/' /etc/nginx/nginx.conf && \
-service nginx restart\"",
+# Fix nginx to accept and serve more requests by increasing worker connections and worker processes
+
+exec { 'increase-worker-connections':
+  command => 'sed -i "s/worker_connections [0-9]\\+/worker_connections 4096/" /etc/nginx/nginx.conf',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+exec { 'increase-worker-processes':
+  command => 'sed -i "s/worker_processes [0-9]\\+/worker_processes auto/" /etc/nginx/nginx.conf',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+exec { 'modify-max-open-files-limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && service nginx restart',
   path    => '/usr/bin:/usr/sbin:/bin',
 }
